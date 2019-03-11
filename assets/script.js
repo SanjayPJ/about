23a636ea458a154a8aa72f312fe528a074c1c4c6
sanjaypj@asus <sanjaypjayan2000@gmail.com>
2019-03-12 01:09:39 +0530

    data mod 1

--- assets/script.js
+++ assets/script.js
@@ -8,3 +8,9 @@ function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
 }
+
+
+document.getElementById("myAnchor").addEventListener("click", function(event){
+    event.preventDefault();
+    document.getElementById('id01').style.display='block';
+  });
